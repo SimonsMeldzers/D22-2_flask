653b14e4dd52cc370e377fefc8e796550a62b9f7
SimonsMeldzers <56105687+SimonsMeldzers@users.noreply.github.com>
2020-02-07 20:09:06 +0200

    file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from file_proc import read_file
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -33,7 +33,24 @@ def read_from_file():
   content = read_file()
   return content
 
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  request_type = request.content_type
+  if request_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Line {contentJSON['data']} added to data.txt"
+  else: 
+    return f"Request ype {request_type} is not supported!"
 
+@app.route('/file', methods = ['GET', 'POST'])
+def fileWork():
+  if request.method =='GET':
+    return read_from_file()
+  elif request.method =='POST':
+    return write_to_file()
+  else:
+    return f"Request method {reques.method} not supported!"
 
 
 
